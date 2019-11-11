1dc00b35250a7fad3f98f1b447f0e95f9630c78f
DangHuan2904 <huandang2904@users.noreply.github.com>
2019-11-11 09:11:39 +0700

    test_game_lan_1

---
+++ src/sample/Main.java
@@ -0,0 +1,41 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    public static int x;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader firstPaneLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent firstPane = firstPaneLoader.load();
+        Scene firstScene = new Scene(firstPane, 1080, 615);
+
+
+        FXMLLoader secondPageLoader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent secondPane = secondPageLoader.load();
+        Scene secondScene = new Scene(secondPane, 1080, 720);
+
+
+        ControllerMain firstPaneController = firstPaneLoader.getController();
+        firstPaneController.setSecondScene(secondScene);
+
+
+        Controller secondPaneController = secondPageLoader.getController();
+        secondPaneController.setFirstScene(firstScene);
+
+        primaryStage.setTitle("Tower Defense");
+        primaryStage.setScene(firstScene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/sample/bottunTower.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.scene.control.Button;
+
+public class bottunTower {
+    Button luffy = new Button("Luffy");
+    Button dragon = new Button("Dragon");
+    Button garp = new Button("Garp");
+
+
+
+
+}
